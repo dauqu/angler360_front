52da4bc60468b8850a185705d68e692c8b3fa5a7
dauqu <sujeetsingh734321@gmail.com>
2023-01-06 17:23:47 +0530

    admin_added

--- src/Constant.js
+++ src/Constant.js
@@ -1,2 +1,2 @@
 // export const API="http://localhost:4000/api";
-export const API = "https://angler360backend-production.up.railway.app/api";
+export const API = "https://angler360backend-production.up.railway.app/api/admin";
